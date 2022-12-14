4ed62a171d562107de03b83933c827b44c751061
GabrielRodriguesMachado <gabrm123@gmail.com>
2022-12-14 20:52:24 -0300

    rotas criadas

--- src/App.jsx
+++ src/App.jsx
@@ -1,55 +1,16 @@
-/* eslint-disable react/jsx-no-bind */
-import React, { useState } from 'react';
-import ChooseWorkout from './components/ChooseWorkout';
-import Workout from './components/Workout';
-import { treinoA, treinoB, treinoC } from './mocks/treinos';
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Login from './pages/Login';
+import Home from './pages/Home';
 
 function App() {
-  const [treino, setTreino] = useState('');
-  const [treinoSelecionado, setTreinoSelecionado] = useState(true);
-  const [treinoEscolhido, setTreinoEscolhido] = useState([]);
-
-  function handleTreinoA() {
-    setTreino('Treino A');
-    setTreinoEscolhido(treinoA);
-    setTreinoSelecionado(false);
-  }
-
-  function handleTreinoB() {
-    setTreino('Treino B');
-    setTreinoEscolhido(treinoB);
-    setTreinoSelecionado(false);
-  }
-
-  function handleTreinoC() {
-    setTreino('Treino C');
-    setTreinoEscolhido(treinoC);
-    setTreinoSelecionado(false);
-  }
-
-  function handleRealizado(id) {
-    const treinoAtualizado = treinoEscolhido.filter((exercicio) => exercicio.id !== id);
-    setTreinoEscolhido(treinoAtualizado);
-  }
   return (
-    <div className="container bg-dark text-light min-vw-100 min-vh-100 pt-4">
-      {
-        treinoSelecionado
-          ? (
-            <ChooseWorkout
-              handleTreinoA={handleTreinoA}
-              handleTreinoB={handleTreinoB}
-              handleTreinoC={handleTreinoC}
-            />
-          ) : (
-            <Workout
-              treino={treino}
-              treinoEscolhido={treinoEscolhido}
-              handleRealizado={handleRealizado}
-            />
-          )
-      }
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/home" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/Home.jsx
@@ -0,0 +1,56 @@
+/* eslint-disable react/jsx-no-bind */
+import React, { useState } from 'react';
+import ChooseWorkout from '../components/ChooseWorkout';
+import Workout from '../components/Workout';
+import { treinoA, treinoB, treinoC } from '../mocks/treinos';
+
+function App() {
+  const [treino, setTreino] = useState('');
+  const [treinoSelecionado, setTreinoSelecionado] = useState(true);
+  const [treinoEscolhido, setTreinoEscolhido] = useState([]);
+
+  function handleTreinoA() {
+    setTreino('Treino A');
+    setTreinoEscolhido(treinoA);
+    setTreinoSelecionado(false);
+  }
+
+  function handleTreinoB() {
+    setTreino('Treino B');
+    setTreinoEscolhido(treinoB);
+    setTreinoSelecionado(false);
+  }
+
+  function handleTreinoC() {
+    setTreino('Treino C');
+    setTreinoEscolhido(treinoC);
+    setTreinoSelecionado(false);
+  }
+
+  function handleRealizado(id) {
+    const treinoAtualizado = treinoEscolhido.filter((exercicio) => exercicio.id !== id);
+    setTreinoEscolhido(treinoAtualizado);
+  }
+  return (
+    <div className="container bg-dark text-light min-vw-100 min-vh-100 pt-4">
+      {
+        treinoSelecionado
+          ? (
+            <ChooseWorkout
+              handleTreinoA={handleTreinoA}
+              handleTreinoB={handleTreinoB}
+              handleTreinoC={handleTreinoC}
+            />
+          ) : (
+            <Workout
+              treino={treino}
+              treinoEscolhido={treinoEscolhido}
+              handleRealizado={handleRealizado}
+            />
+          )
+      }
+    </div>
+  );
+}
+
+export default App;

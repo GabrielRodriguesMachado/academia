e1954e7c85c02c7a1c0a6359377aa5ecfdb5e98f
GabrielRodriguesMachado <gabrm123@gmail.com>
2022-12-14 12:41:14 -0300

    eslint configurado

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,9 @@
-import { useState } from 'react'
+import React from 'react';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div id="carouselExampleControls" className="carousel slide" data-bs-ride="carousel">
-  <a href="#" className='btn btn-primary'>botao</a>
-</div>
-  )
+    <div id="carouselExampleControls" className="carousel slide" data-bs-ride="carousel" />
+  );
 }
 
-export default App
+export default App;

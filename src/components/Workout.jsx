ffb73fe9fbc70afab17989269c29b9d45ee0e5f6
GabrielRodriguesMachado <gabrm123@gmail.com>
2022-12-14 16:53:07 -0300

    carousel criado

--- src/components/Workout.jsx
+++ src/components/Workout.jsx
@@ -1,40 +1,44 @@
 /* eslint-disable react/prop-types */
-import React from 'react';
+import React, { useState } from 'react';
+import { Carousel } from 'react-bootstrap';
 
-function Workout({ treino, treinoEscolhido, handleRealizado }) {
+function Workout({ treino, treinoEscolhido }) {
+  const [index, setIndex] = useState(0);
+  const handleSelect = (selectedIndex) => setIndex(selectedIndex);
   return (
     <div className="text-center">
       <h1>{treino}</h1>
-      {treinoEscolhido.map((exercicio) => (
-        <div key={exercicio.id}>
-          <p>{exercicio.nome}</p>
-          <p>
-            Séries:
-            {' '}
-            {exercicio.series}
-          </p>
-          <p>
-            Repetições
-            {' '}
-            {exercicio.repeticoes}
-          </p>
-          <p>
-            Carga:
-            {' '}
-            {exercicio.carga}
-            kg
-          </p>
-          {/* <p className="text-gray-200 text-base">Realizado?</p>
-            <input type="checkbox" name="" id="" className="text-gray-700 text-base" /> */}
-          <button
-            onClick={() => handleRealizado(exercicio.id)}
-            type="button"
-          >
-            Realizado
-          </button>
-          <hr />
-        </div>
-      ))}
+      <Carousel
+        activeIndex={index}
+        onSelect={handleSelect}
+        interval={null}
+        indicators={false}
+        controls={false}
+      >
+        {treinoEscolhido.map((exercicio) => (
+          <Carousel.Item key={exercicio.id}>
+            <div>
+              <p>{exercicio.nome}</p>
+              <p>
+                Séries:
+                {' '}
+                {exercicio.series}
+              </p>
+              <p>
+                Repetições
+                {' '}
+                {exercicio.repeticoes}
+              </p>
+              <p>
+                Carga:
+                {' '}
+                {exercicio.carga}
+                kg
+              </p>
+            </div>
+          </Carousel.Item>
+        ))}
+      </Carousel>
     </div>
   );
 }
